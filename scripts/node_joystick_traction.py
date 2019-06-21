9d49dae67d4e58ae29b03fd18c4050629b108d61
jdserranomu <jd.serranomu@uniandes.edu.co>
2019-06-20 23:33:40 -0500

    Convencion de minuscular en topicos y nodos

--- scripts/node_joystick_traction.py
+++ scripts/node_joystick_traction.py
@@ -3,13 +3,13 @@ import rospy
 from master_msgs.msg import traction_Orders
 
 
-def node_Joystick_Traction():
+def node_joystick_traction():
 
-    rospy.init_node('node_Joystick_Traction',anonymous=True)
+    rospy.init_node('node_joystick_traction',anonymous=True)
     
 
     #publica en el topico traction orders
-    pub_Traction_Orders = rospy.Publisher('topic_Traction_Orders', traction_Orders, queue_size=10)
+    pub_Traction_Orders = rospy.Publisher('topic_traction_orders', traction_Orders, queue_size=10)
     rate = rospy.Rate (10)
     while not rospy.is_shutdown ():
         rate.sleep ()
@@ -18,7 +18,7 @@ def node_Joystick_Traction():
 if __name__ == '__main__':
     try:
        
-        node_Joystick_Traction()
+        node_joystick_traction()
         rate = rospy.Rate (10)
 
     except rospy.ROSInterruptException:
